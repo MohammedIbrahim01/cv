a2787f504e06ace03b093e42f3d3ee1dde9c7510
X <mohammedibrahim01@gmail.com>
2018-09-08 00:21:09 +0200

    initial commit

---
+++ app/src/main/java/com/example/x/cv/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.x.cv;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private EditText first, second, email, pass;
+    private Button submit;
+
+    private CheckBox rem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+        if (sharedPreferences.getString("data", "").equals("done")) {
+
+            startActivity(new Intent(MainActivity.this, Welcome.class));
+            finish();
+        }
+
+
+        calling();
+        handling();
+    }
+
+    private void calling() {
+
+        first = (EditText) findViewById(R.id.first_name_editText);
+        second = (EditText) findViewById(R.id.last_name_editText);
+        email = (EditText) findViewById(R.id.email_editText);
+        pass = (EditText) findViewById(R.id.password_editText);
+
+        submit = (Button) findViewById(R.id.submit_button);
+
+        rem = (CheckBox) findViewById(R.id.rem_check);
+    }
+
+    void handling() {
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                sharedPreferences.edit().putString("first", first.getText().toString()).commit();
+                sharedPreferences.edit().putString("second", second.getText().toString()).commit();
+                sharedPreferences.edit().putString("email", email.getText().toString()).commit();
+                sharedPreferences.edit().putString("pass", pass.getText().toString()).commit();
+                sharedPreferences.edit().putString("data", "done").commit();
+
+                Intent i = new Intent(MainActivity.this, Welcome.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/x/cv/Welcome.java
@@ -0,0 +1,102 @@
+package com.example.x.cv;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class Welcome extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    TextView welcome;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        calling();
+        handling();
+    }
+
+    void calling() {
+
+        welcome = findViewById(R.id.welcome_textView);
+        webView = findViewById(R.id.webView);
+    }
+
+    void handling() {
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Welcome.this);
+
+        String fullName = "";
+
+        fullName = sharedPreferences.getString("first", "") + " " + sharedPreferences.getString("second", "");
+
+        welcome.append(fullName);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_facebook) {
+
+            Uri facebook = Uri.parse("https://www.facebook.com");
+            Intent faceIntent = new Intent(Intent.ACTION_VIEW, facebook);
+            startActivity(faceIntent);
+            return true;
+        } else if (item.getItemId() == R.id.action_google) {
+
+            webView.setWebViewClient(new webView());
+            webView.loadUrl("https://www.google.com");
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+            return true;
+        } else if (item.getItemId() == R.id.action_linkedIn) {
+
+
+            webView.setWebViewClient(new webView());
+            webView.loadUrl("https://www.linkedIn.com");
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+
+
+            return true;
+
+        }else if (item.getItemId() == R.id.action_previous_education) {
+
+
+
+            return true;
+
+        }else if (item.getItemId() == R.id.action_previous_work) {
+
+
+            return true;
+
+        }else if (item.getItemId() == R.id.action_memories) {
+
+
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
