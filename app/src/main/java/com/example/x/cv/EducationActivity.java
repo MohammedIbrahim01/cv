1729caccf73087d994ea9a13c982b1ba45f26c4c
Abdelaziz <Abdelaziz.elantaky@Gmail.com>
2018-09-13 22:09:05 +0200

    optimization of education activity and revision of education listView

--- app/src/main/java/com/example/x/cv/EducationActivity.java
+++ app/src/main/java/com/example/x/cv/EducationActivity.java
@@ -2,7 +2,6 @@ package com.example.x.cv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,38 +14,35 @@ public class EducationActivity extends AppCompatActivity {
     String academicDegreeTextView[];
     String gradeTextView[];
     String donorTextView[];
-    int logoForDegree[] = {R.drawable.zlogo, R.drawable.zlogo, R.drawable.zlogo, R.drawable.zlogo, R.drawable.zlogo, R.drawable.zlogo};
+
+    int logoForDegree[]={R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,R.drawable.zlogo,};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
-        fechArrays();
+        fetchArrays();
+
         ArrayList<CustomObjectForArrays> educationHistory = new ArrayList<CustomObjectForArrays>();
-        int index = 0;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index++;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index++;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index++;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index++;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index++;
-        educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
-        index=0;
+        for (int index = 0; index < getResources().getStringArray(R.array.fromDateTextView).length;index++) {
+            logoForDegree[index] = R.drawable.zlogo;
+            educationHistory.add(new CustomObjectForArrays(fromDataTextView[index], toDataTextView[index], specializationTextView[index], academicDegreeTextView[index], gradeTextView[index], donorTextView[index], logoForDegree[index]));
+        }
         AdapterForCustomListViews adapter = new AdapterForCustomListViews(EducationActivity.this,R.layout.education_item_list,educationHistory);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(adapter);
-
     }
-    public void fechArrays(){
-        fromDataTextView = getResources().getStringArray(R.array.fromDataTextView);
-        toDataTextView = getResources().getStringArray(R.array.toDataTextView);
+    public void fetchArrays(){
+
+
+
+        fromDataTextView = getResources().getStringArray(R.array.fromDateTextView);
+        toDataTextView = getResources().getStringArray(R.array.toDateTextView);
          specializationTextView = getResources().getStringArray(R.array.specializationTextView);
         academicDegreeTextView = getResources().getStringArray(R.array.academicDegreeTextView);
          gradeTextView = getResources().getStringArray(R.array.gradeTextView);
          donorTextView = getResources().getStringArray(R.array.donorTextView);
     }
+
 }
